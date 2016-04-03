c3f3448d6cab922473806179dddf96f114bbe2fc
yangsongsong <yangsong@32gold.com>
2016-04-03 22:33:09 +0800

    项目提交

---
+++ app/src/main/java/com/ys/administrator/eventbusproject/MessageEventOne.java
@@ -0,0 +1,13 @@
+package com.ys.administrator.eventbusproject;
+
+/**
+ * Created by Administrator on 2016/3/10.
+ */
+public class MessageEventOne {
+
+    public String message;
+
+    public MessageEventOne(String message){
+        this.message = message;
+    }
+}
